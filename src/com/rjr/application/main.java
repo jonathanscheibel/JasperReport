ccd6758ea01e9ce541f945a286eacf42e7030ca0
ricardojr01 <ricardojr01@hotmail.com>
2016-07-08 14:02:35 -0300

    Novas implementações.

--- src/com/rjr/application/main.java
+++ src/com/rjr/application/main.java
@@ -4,13 +4,12 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.sql.Connection;
-//import java.util.HashMap;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 import com.wals.jdbc.ConnectionFactory;
 
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
@@ -19,27 +18,19 @@ import net.sf.jasperreports.view.JasperViewer;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ConnectionFactory fabrica = new ConnectionFactory();
-		Connection con = fabrica.getInstance();
-//		HashMap<String, Object> param = new HashMap<String, Object>();
-		
-		try {
-//			JasperCompileManager.compileReportToFile("src/com/rjr/report/Blank_A4.jrxml");
-			
-		
-//			JasperPrint jp = JasperFillManager.fillReport("src/com/rjr/report/Blank_A4.jasper", null, con);
-//			JasperPrint jp = JasperFillManager.fillReport("C:\\Users\\Ricardo\\workspace\\jasperReport\\src\\com\\rjr\\report\\Blank_A4.jasper", null, con);
+	public static void printReport(Connection conn){
+		try {	
+//			HashMap param = new HashMap();
+//			JasperPrint jp = JasperFillManager.fillReport("src/com/rjr/report/ReportUser.jasper", param, con);
 //			JasperViewer jw = new JasperViewer(jp);
-//			jw.setVisible(true);
+//			jw.setVisible(true);			          			
 			
-            FileInputStream fis = new FileInputStream("C:\\Users\\Ricardo\\workspace\\jasperReport\\src\\com\\rjr\\report\\Blank_A4.jasper");
+			FileInputStream fis = new FileInputStream("C:\\Users\\Ricardo\\workspace\\jasperReport\\src\\com\\rjr\\report\\ReportUser.jasper");
             BufferedInputStream bufferedInputStream = new BufferedInputStream(fis);
-
+            
             //compile report
             JasperReport jasperReport = (JasperReport) JRLoader.loadObject(bufferedInputStream);
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, con);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, conn);
  
             // view report to UI
             JasperViewer.viewReport(jasperPrint, false);
@@ -51,6 +42,53 @@ public class main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+	
+	public static void printReportParam(Connection conn, @SuppressWarnings("rawtypes") HashMap param){
+		try {				          						
+			FileInputStream fis = new FileInputStream("C:\\Users\\Ricardo\\workspace\\jasperReport\\src\\com\\rjr\\report\\ReportUserParam.jasper");
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fis);
+            
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(bufferedInputStream);
+            @SuppressWarnings("unchecked")
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, param, conn);
+            JasperViewer.viewReport(jasperPrint, false);
+
+		} catch (JRException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConnectionFactory fabrica = new ConnectionFactory();
+		Connection con = fabrica.getInstance();
+		
+		//Report sem parametros
+//		printReport(con);
+		
+		//Criando parametros
+		HashMap param = new HashMap();
+        param.put("grupo", 1);
+		
+        //Report com paramentros
+        printReportParam(con, param);
+        
+		
+		System.out.println("Fechando conexao");		
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
